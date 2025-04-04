db3e47ec227d2acad64a38ee72c57c7614a1ebeb
rfof-network.org <176731182+RFOF-NETWORK@users.noreply.github.com>
2025-04-04 02:16:43 +0200

    Aktualisieren von scripts.js

--- website/static/scripts.js
+++ website/static/scripts.js
@@ -15,3 +15,20 @@ function sendMessage() {
         document.getElementById("response-output").innerText = "Fehler: " + error;
     });
 }
+
+function loadData(storageType) {
+    fetch(`/view/${storageType}?user=Satoramy`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Zugriff verweigert! Nur Satoramy darf die Daten sehen.");
+            }
+            return response.json();
+        })
+        .then(data => {
+            const container = document.getElementById(`${storageType}net-data`);
+            container.innerHTML = JSON.stringify(data, null, 2);
+        })
+        .catch(error => {
+            alert(error.message);
+        });
+}
