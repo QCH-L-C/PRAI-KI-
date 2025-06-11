d5325113b325d8f7152a340ba49efb93887a6c56
rfof-network.org = rfof-bitcoin.org - = rfofspidernet.de <176731182+RFOF-NETWORK@users.noreply.github.com>
2025-06-12 00:31:31 +0200

    Update menu_script.js

--- menu_script.js
+++ menu_script.js
@@ -2,10 +2,20 @@ document.addEventListener('DOMContentLoaded', function() {
     const hamburger = document.getElementById('hamburger');
     const navLinks = document.getElementById('navLinks');
 
-    if (hamburger && navLinks) { // Sicherstellen, dass Elemente existieren
+    if (hamburger && navLinks) {
         hamburger.addEventListener('click', function() {
             navLinks.classList.toggle('active');
             hamburger.classList.toggle('active');
         });
     }
+
+    // Close menu when a link is clicked (especially for mobile)
+    navLinks.querySelectorAll('a').forEach(link => {
+        link.addEventListener('click', () => {
+            if (navLinks.classList.contains('active')) {
+                navLinks.classList.remove('active');
+                hamburger.classList.remove('active');
+            }
+        });
+    });
 });
